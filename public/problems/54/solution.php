a72cb7ea63fdd0f65b2dbcc58d2b80de9f45ef2c
lucastdcj <lucastdcj@gmail.com>
2017-05-30 21:30:33 -0300

    Update solution.php

--- public/problems/54/solution.php
+++ public/problems/54/solution.php
@@ -1,11 +1,18 @@
-<center><i>(OBI2012, Fase 1, Nível Júnior)</i></center>
-<h1 align="center">Solução - Busca na Internet</h1>
+<h1> Solução  - Corrida</h1>
 
-<pre class="code">
-  int main() {
-    int x;
-    scanf(" %d",&x);
-    printf("%d\n",4*x);
-    return 0;
-  }
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0ucGfbvAkRc" frameborder="0" allowfullscreen></iframe>
+<br />
+<h2> Código </h2>
+<pre class="prettyprint"><?php echo htmlspecialchars('
+#include <cstdio>
+
+int main() {
+  int a, b;
+  int custo_por_km, valor_pedagio;
+  scanf(" %d %d %d %d", &a, &b, &custo_por_km, &valor_pedagio);
+  printf("%d\n", (a / b) * valor_pedagio + a * custo_por_km);
+  return 0;
+}');?>   
 </pre>
+<script src="http://93.188.167.1/public/highlighter/prettify.js"></script>
+<script>prettyPrint();</script>
