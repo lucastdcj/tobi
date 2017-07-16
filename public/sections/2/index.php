f20bff36d72c0f4b63050809565d2fcc04fbe3fc
lucastdcj <lucastdcj@gmail.com>
2017-07-16 18:33:57 -0300

    Update index.php

--- public/sections/2/index.php
+++ public/sections/2/index.php
@@ -4,7 +4,7 @@
 <a href="http://olimpiada.ic.unicamp.br/saci/cursos_u/intro/class?c=intro_7"> Saci - Aula 7</a> <br>
 <a href="http://olimpiada.ic.unicamp.br/extras/cursoC/Cap05-EstruturasCondicionais-texto.pdf"> Estruturas Condicionais - Arnaldo V. Moura, Daniel F. Ferber</a> <br>
 
-<h2> Problemas Exemplo </h2>
+<h2> Problema Exemplo </h2>
 <b>Sedex: </b> Neste problema é dado o diâmetro de uma bola de boliche e as dimensões de uma caixa. Para saber se a bola cabe na caixa, basta verificar se o diâmetro é menor do que todas as medidas da caixa.<br>
 Solução:
 <pre class="prettyprint"><?php echo htmlspecialchars('
