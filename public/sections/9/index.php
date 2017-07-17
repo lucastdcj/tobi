9a9250d7d4c8984cb68fb623e5f3ee01b5976c54
lucastdcj <lucastdcj@gmail.com>
2017-07-17 08:07:24 -0300

    Update index.php

--- public/sections/9/index.php
+++ public/sections/9/index.php
@@ -1,8 +1,8 @@
 <h1> 3.1 - Ad-hoc </h1>
 
-<p>Cada problema Ad-Hoc é único, diferente de outros problemas, e nenhuma solução ou técnica geral existe para resolvê-los.</p> <br>
+<p>Cada problema Ad-Hoc é único, diferente de outros problemas, e nenhuma solução ou técnica geral existe para resolvê-los.</p>
 
-<p>Bem, é bom encontrar esses problemas, pois seu cérebro será moldado de forma a quebrar um problema de muitas maneiras diferentes. As soluções podem exigir uma nova estrutura de dados criada pelo usuário, ou algum tipo de combinação especial, ou conjunto de loops ou condições incomuns.</p> <br>
+<p>Bem, é bom encontrar esses problemas, pois seu cérebro será moldado de forma a quebrar um problema de muitas maneiras diferentes. As soluções podem exigir uma nova estrutura de dados criada pelo usuário, ou algum tipo de combinação especial, ou conjunto de loops ou condições incomuns.</p>
 
-<p>Ao resolver problemas Ad-Hoc, você deve ler o problema com muito cuidado e produzir uma solução que satisfaça todas as restrições e condições mencionadas no enunciado.</p> <br>
+<p>Ao resolver problemas Ad-Hoc, você deve ler o problema com muito cuidado e produzir uma solução que satisfaça todas as restrições e condições mencionadas no enunciado.</p>
 
