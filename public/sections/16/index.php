7da05ad36ad605327f2650e325e70aab051e8f55
lucastdcj <lucastdcj@gmail.com>
2017-07-16 23:20:59 -0300

    Update index.php

--- public/sections/16/index.php
+++ public/sections/16/index.php
@@ -1,4 +1,4 @@
 <h1> 4.3 - Árvore e suas propriedades </h1>
 <h2> Material </h2>
-<a href="https://pt.wikipedia.org/wiki/%C3%81rvore_(grafo)"> Árvore - Wikipedia </a>
+<a href="https://pt.wikipedia.org/wiki/%C3%81rvore_(grafo)"> Árvore - Wikipedia </a> <br>
 <a href="http://noic.com.br/informatica/curso-noic-de-informatica/aula-14-grafos-iv-lca-menor-ancestral-comum/"> Menor Ancestral Comum - NOIC </a> <br>
