c7ba88ee942d4739cf17b4a095b3b463cfa9ff67
lucastdcj <lucastdcj@gmail.com>
2017-07-16 22:55:01 -0300

    Update index.php

--- public/sections/15/index.php
+++ public/sections/15/index.php
@@ -1,1 +1,4 @@
-<h1> Em construção </h1>
+<h1> 4.2 - Busca em Largura e Profundidade</h1>
+<h2> Material </h2>
+<a href="https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/bfs.html"> Busca em largura (BFS) - IME USP </a> <br>
+<a href="https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/dfs.html"> Busca em profundidade (DFS) - IME USP </a> <br>
