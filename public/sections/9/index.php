aa006ee3bce3c9700dc262207fa2af882b83d690
lucastdcj <lucastdcj@gmail.com>
2017-07-17 08:05:17 -0300

    Update index.php

--- public/sections/9/index.php
+++ public/sections/9/index.php
@@ -1,11 +1,11 @@
 <h1> 3.1 - Ad-hoc </h1>
 
-Existem alguns problemas, cujos algoritmos não se enquadram em soluções bem estudadas de categorias padrão.
+<p>Existem alguns problemas, cujos algoritmos não se enquadram em soluções bem estudadas de categorias padrão.</p>
 
-Ad-Hoc cai em um deles. Cada problema Ad-Hoc é único, diferente de outros problemas, e nenhuma solução ou técnica geral existe para resolvê-los.
+<p>Ad-Hoc cai em um deles. Cada problema Ad-Hoc é único, diferente de outros problemas, e nenhuma solução ou técnica geral existe para resolvê-los.</p>
 
-Bem, é bom encontrar esses problemas, pois seu cérebro será moldado de forma a quebrar um problema de muitas maneiras diferentes. As soluções podem exigir uma nova estrutura de dados criada pelo usuário, ou algum tipo de combinação especial, ou conjunto de loops ou condições incomuns.
+<p>Bem, é bom encontrar esses problemas, pois seu cérebro será moldado de forma a quebrar um problema de muitas maneiras diferentes. As soluções podem exigir uma nova estrutura de dados criada pelo usuário, ou algum tipo de combinação especial, ou conjunto de loops ou condições incomuns.</p>
 
-Ao resolver problemas Ad-Hoc, você deve ler o problema com muito cuidado e produzir uma solução que satisfaça todas as restrições e condições mencionadas na Declaração do problema.
+<p>Ao resolver problemas Ad-Hoc, você deve ler o problema com muito cuidado e produzir uma solução que satisfaça todas as restrições e condições mencionadas na Declaração do problema.</p>
 
-No entanto, todos e cada um dos problemas exigem uma nova solução, é obrigatório ter em mente que os trade-off do tempo e do espaço ainda são mantidos. Para isso, você precisa otimizar seu código e criar uma solução eficiente.
+<p>No entanto, todos e cada um dos problemas exigem uma nova solução, é obrigatório ter em mente que os trade-off do tempo e do espaço ainda são mantidos. Para isso, você precisa otimizar seu código e criar uma solução eficiente.</p>
